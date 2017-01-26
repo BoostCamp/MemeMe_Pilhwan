099d56e75991f4e377af759e8940d48eceea2ea6
pilhwanKim <pilhwankim03@gmail.com>
2017-01-26 11:36:57 +0900

    init storyboard for collection and table views

---
+++ MemeMe/MemeMeCollectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CollectionViewController.swift
+//  MemeMe
+//
+//  Created by 김필환 on 2017. 1. 26..
+//  Copyright © 2017년 김필환. All rights reserved.
+//
+
+import UIKit
+
+class MemeMeCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
